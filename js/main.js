38ea2d6d07241b9af128c6c19ba9c83beaa26e11
yakuzzaa <chornaya.gryazz@gmail.com>
2022-08-04 21:07:45 +0300

    attached event listener for all cards

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
 "use strict"
-
 const $card1 = document.querySelector(".card1")
-// const $card1_rectangle = document.querySelector('.')
 const $card2 = document.querySelector(".card2")
 const $card3 = document.querySelector(".card3")
-$card1.addEventListener("click", function (e) {
-    const $corner = $card1.querySelector('.card_title .corner')
-    const $card_rectangle = $card1.querySelector('.card_title .rectangle')
-    const $card_object = $card1.querySelector('.card_object')
-    const $card_ellipse = $card1.querySelector('.size_ellipse')
+
+const updated_text_for_cards ={
+    'card1': 'Печень утки разварная с артишоками.',
+    'card2': 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+    'card3': 'Филе из цыплят с трюфелями в бульоне.'
+}
+
+function clickEvent($card){
+    const $corner = $card.querySelector('.card_title .corner')
+    const $card_rectangle = $card.querySelector('.card_title .rectangle')
+    const $card_object = $card.querySelector('.card_object')
+    const $card_ellipse = $card.querySelector('.size_ellipse')
+    const $sell_text = $card.querySelector('.sell_text')
     $corner.classList.add('selected_corner')
     $card_rectangle.classList.add('selected_rectangle')
     $card_object.classList.add('selected_card')
     $card_ellipse.classList.add('selected_ellipse')
-    console.log('fuck')
+    $sell_text.innerText = updated_text_for_cards[$card.id]
+    console.log($card)
+}
+
+
+
+$card1.addEventListener("click", function (e) {
+    clickEvent($card1)
+})
+$card2.addEventListener("click", function (e) {
+    clickEvent($card2)
+})
+$card3.addEventListener("click", function (e) {
+    clickEvent($card3)
 })
